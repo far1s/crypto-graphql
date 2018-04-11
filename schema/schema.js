8fba38bf3a5c4996c3f63ff82861b646feb01edd
Faris Brni <farisbrni@gmail.com>
2018-04-11 23:42:01 +0200

    Add files

---
+++ schema/schema.js
@@ -0,0 +1,53 @@
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList
+} = require('graphql');
+const axios = require('axios');
+
+const CoinType = new GraphQLObjectType({
+    name: 'Coin',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        symbol: { type: GraphQLString },
+        rank: { type: GraphQLString },
+        price_usd: { type: GraphQLString },
+        price_btc: { type: GraphQLString },
+        h24_volume_usd: { type: GraphQLString },
+        market_cap_usd: { type: GraphQLString },
+        available_supply: { type: GraphQLString },
+        total_supply: { type: GraphQLString },
+        percent_change_1h: { type: GraphQLString },
+        percent_change_24h: { type: GraphQLString },
+        percent_change_7d: { type: GraphQLString }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        coins: {
+            type: new GraphQLList(CoinType),
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(_, {id}) {
+                const url = id
+                    ? `https://api.coinmarketcap.com/v1/ticker/${id}`
+                    : 'https://api.coinmarketcap.com/v1/ticker/';
+                return axios.get(url).then(
+                    res => res.data
+                ).catch(
+                    error => console.log('error', error)
+                );
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const expressGraphQl = require('express-graphql');
+const schema = require('./schema/schema');
+const axios = require('axios');
+
+
+const app = express();
+
+app.use('/graphql', expressGraphQl({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('http://localhost:4000');
+});
